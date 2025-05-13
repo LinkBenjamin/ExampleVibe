ff96817ad7fb2fe93d10c35e65a9b0ea2d67266f
Ben Link <LinkBenjamin@users.noreply.github.com>
2025-05-13 19:50:17 -0400

    Update Cyclomatic.py

--- Cyclomatic.py
+++ Cyclomatic.py
@@ -10,10 +10,11 @@ def is_strong_password(password):
     - Contains at least one uppercase letter
     - Contains at least one digit
     - Contains at least one special character
+    - Does NOT contain simple repeated patterns (e.g., 'abcabc', '123123', 'aaaaaa')
     """
     if len(password) < 8:
         return False
-    
+
     if not re.search(r'[a-z]', password):
         return False
 
@@ -24,6 +25,17 @@ def is_strong_password(password):
         return False
 
     if not re.search(r'[!@#$%^&*()\-\_\+=]', password):
+        return False
+
+    # Check for repeated sequences (2-6 character sequences)
+    for size in range(2, 7):
+        for i in range(len(password) - 2 * size + 1):
+            chunk = password[i:i+size]
+            if chunk * 2 in password:
+                return False
+
+    # Check for any character repeated 4 or more times consecutively
+    if re.search(r'(.)\1{3,}', password):
         return False
 
     return True
